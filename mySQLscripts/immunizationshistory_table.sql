18cfce2386998caa05f003a51520ff49d384b105
Sharyl Riley <68468609+sriley86@users.noreply.github.com>
2021-04-04 15:17:56 -0400

    Added Extra Tables for Group of 3 People /Extra Credit

---
+++ mySQLscripts/immunizationshistory_table.sql
@@ -0,0 +1,17 @@
+/*its 340 - Spring 2021*/
+CREATE SCHEMA IF NOT EXISTS genmedhis;
+use genmedhis;
+drop table if exists immunizationshistorytable;
+CREATE TABLE `immunizationshistorytable` ( 
+`ImmunizationsID` int(11) NOT NULL AUTO_INCREMENT, 
+`PatientID` int(11) DEFAULT NULL,
+`Vaccine` varchar(128) DEFAULT NULL, 
+`ImmunizationDate` date DEFAULT NULL, 
+`ExperationDate` date DEFAULT NULL,
+`Delivery` varchar(128) DEFAULT NULL,
+`Comments` varchar(254) DEFAULT NULL,
+`HCPId` int(11) DEFAULT NULL,
+`deleted` tinyint(1) DEFAULT '0',
+PRIMARY KEY (`ImmunizationsID`),
+KEY `I_PatientID` (`PatientID`)
+);
